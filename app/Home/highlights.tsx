d18f9197b1e36bbc4d8a61bd7ed1e6743d148800
AbdulAzeez Bello <abdulazeezbello5882@gmail.com>
2026-03-06 13:51:50 +0100

    updated hight

--- app/Home/highlights.tsx
+++ app/Home/highlights.tsx
@@ -44,6 +44,97 @@ const VIDEOS = [
     username: 'Prof Abdullahi Bappa Garkuwa',
     title: 'Ta ziyyar rasuwar Alaramma Usamah Ahmad Musa wanda ya rasu a Jami\'ar Islamiyya a Birnin Madina Allah Ya masa rahama da sauran magabatanmu',
   },
+  {
+    id: '7',
+    url: 'https://res.cloudinary.com/dpjni6fdl/video/upload/v1772799627/Allah_yasa_mu_Dace_Duniya_Da_Lahira_iv3oh8.mp4',
+    username: 'Prof Abdullahi Bappa Garkuwa',
+    title: 'Allah yasa mu Dace Duniya Da Lahira',
+  },
+  {
+    id: '8',
+    url: 'https://res.cloudinary.com/dpjni6fdl/video/upload/v1772799555/Qur_ani_Ya_Kunshi_Komai_wtntw7.mp4',
+    username: 'Prof Abdullahi Bappa Garkuwa',
+    title: 'Qur ani Ya Kunshi Komai',
+  },
+  {
+    id: '9',
+    url: 'https://res.cloudinary.com/dpjni6fdl/video/upload/v1772799499/Allah_mai_halittu_kala_kala_jwbzx2.mp4',
+    username: 'Prof Abdullahi Bappa Garkuwa',
+    title: 'Allah mai halittu kala kala',
+  },
+  {
+    id: '10',
+    url: 'https://res.cloudinary.com/dpjni6fdl/video/upload/v1772799454/Mu_yawaita_Ayyukan_Alkhairi_apcqez.mp4',
+    username: 'Prof Abdullahi Bappa Garkuwa',
+    title: 'Mu yawaita Ayyukan Alkhairi',
+  },
+  {
+    id: '11',
+    url: 'https://res.cloudinary.com/dpjni6fdl/video/upload/v1772799419/RAMADN_TAFSEER_KAGARAWAL_ib1v0b.mp4',
+    username: 'Prof Abdullahi Bappa Garkuwa',
+    title: 'RAMADN TAFSEER KAGARAWAL',
+  },
+  {
+    id: '12',
+    url: 'https://res.cloudinary.com/dpjni6fdl/video/upload/v1772799290/Ramadan_Tafseer_Moments_at_Kagarawal_Kofar_Dagaci_Jum_at_Mosque_nl4cfa.mp4',
+    username: 'Prof Abdullahi Bappa Garkuwa',
+    title: 'Ramadan Tafseer Moments at Kagarawal Kofar Dagaci Jum at Mosque',
+  },
+  {
+    id: '13',
+    url: 'https://res.cloudinary.com/dpjni6fdl/video/upload/v1772799281/ISRAILIYYAT_qwxzb8.mp4',
+    username: 'Prof Abdullahi Bappa Garkuwa',
+    title: 'ISRAILIYYAT',
+  },
+  {
+    id: '14',
+    url: 'https://res.cloudinary.com/dpjni6fdl/video/upload/v1772799295/Ameen_Ya_ALLAH_etyaqs.mp4',
+    username: 'Prof Abdullahi Bappa Garkuwa',
+    title: 'Ameen Ya ALLAH',
+  },
+  {
+    id: '15',
+    url: 'https://res.cloudinary.com/dpjni6fdl/video/upload/v1772799307/RAMADAN_MOMENT_rkvbew.mp4',
+    username: 'Prof Abdullahi Bappa Garkuwa',
+    title: 'RAMADAN MOMENT',
+  },
+  {
+    id: '16',
+    url: 'https://res.cloudinary.com/dpjni6fdl/video/upload/v1772799319/Allah_ya_kyauta_cqokfm.mp4',
+    username: 'Prof Abdullahi Bappa Garkuwa',
+    title: 'Allah ya kyauta',
+  },
+  {
+    id: '17',
+    url: 'https://res.cloudinary.com/dpjni6fdl/video/upload/v1772799322/Mu_koma_ga_Allah_cdtivs.mp4',
+    username: 'Prof Abdullahi Bappa Garkuwa',
+    title: 'Mu koma ga Allah',
+  },
+  {
+    id: '18',
+    url: 'https://res.cloudinary.com/dpjni6fdl/video/upload/v1772799347/2026_RAMADAN_MOMENTS_irqyuw.mp4',
+    username: 'Prof Abdullahi Bappa Garkuwa',
+    title: '2026 RAMADAN MOMENTS',
+  },
+  {
+    id: '19',
+    url: 'https://res.cloudinary.com/dpjni6fdl/video/upload/v1772799380/Allah_Ya_Inganta_Mana_Rayuwarmu_pl6lov.mp4',
+    username: 'Prof Abdullahi Bappa Garkuwa',
+    title: 'Allah Ya Inganta Mana Rayuwarmu',
+  },
+  {
+    id: '20',
+    url: 'https://res.cloudinary.com/dpjni6fdl/video/upload/v1772799388/Duniya_Labari_Allah_yasa_Muyi_Kyakkyawan_Karshe_epfxse.mp4',
+    username: 'Prof Abdullahi Bappa Garkuwa',
+    title: 'Duniya Labari Allah yasa Muyi Kyakkyawan Karshe',
+  },
+  {
+    id: '21',
+    url: 'https://res.cloudinary.com/dpjni6fdl/video/upload/v1772799388/Duniya_Labari_Allah_yasa_Muyi_Kyakkyawan_Karshe_epfxse.mp4',
+    username: 'Prof Abdullahi Bappa Garkuwa',
+    title: 'Duniya Labari Allah yasa Muyi Kyakkyawan Karshe',
+  },
+
 
 
 ];
